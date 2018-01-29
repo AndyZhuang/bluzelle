b54e8981f24a3ab9558c00e69550cba8d4cb0ac9
Monty Thibault <montythibault@gmail.com>
2018-01-29 14:03:43 -0800

    Styling changes to header.

--- client/crud/web/src/components/Main.js
+++ client/crud/web/src/components/Main.js
@@ -1,17 +1,25 @@
 import {JSONEditor} from "./JSONEditor";
 import {getSwarmData} from '../services/DataService';
 import {KeyList} from "./KeyList";
+import logo from './logo-color.png';
 import 'bootstrap/dist/css/bootstrap.css';
 
 
 const MainComponent = () => (
-    <ReflexContainer
-        style={{height: '100%'}}>
-
-        <ReflexFixed>
-            <h1>Database</h1>
+    <ReflexContainer style={{ height: '100%' }}>
+        <ReflexFixed style={{
+            marginBottom: 15
+        }}>
+            <img src={logo}/>
+            <h1 style={{
+                display: 'inline-block',
+                fontWeight: 'bold',
+                marginLeft: 35,
+                marginBottom: 0,
+                verticalAlign: 'bottom',
+                fontSize: 40
+            }}>Database Editor</h1>
         </ReflexFixed>
-
         <ReflexElement flex={1}>
             <ReflexContainer orientation='vertical'>
                 <ReflexElement flex={0.4}>
